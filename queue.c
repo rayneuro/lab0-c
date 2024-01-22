5a1b7828bd971f87c49489194c5e633d2675ac43
ray <ray1997ray75@gmail.com>
2024-01-23 03:02:07 +0800

    Save Jan.1

--- queue.c
+++ queue.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 #include "queue.h"
-
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
  * following line.
@@ -12,6 +13,13 @@
 
 
 /* Create an empty queue */
+
+typedef bool (*list_cmp_func_t)(void *,
+                                const struct list_head *,
+                                const struct list_head *);
+
+
+
 struct list_head *q_new()
 {
     // queue_contex_t * t = malloc(sizeof(queue_contex_t));
@@ -299,6 +307,8 @@ bool q_delete_dup(struct list_head *head)
     return true;
 }
 
+
+
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
@@ -314,17 +324,190 @@ void q_reverseK(struct list_head *head, int k)
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
 }
 
+
+bool cmp(void *priv, const struct list_head *a, const struct list_head *b)
+{
+    if (*(bool *) priv == true) {
+        if (strcmp(container_of(a, element_t, list)->value,
+                   container_of(b, element_t, list)->value) < 0)
+            return false;
+        else if (strcmp(container_of(a, element_t, list)->value,
+                        container_of(b, element_t, list)->value) > 0)
+            return true;
+    } else {
+        if (strcmp(container_of(a, element_t, list)->value,
+                   container_of(b, element_t, list)->value) < 0)
+            return true;
+        else if (strcmp(container_of(a, element_t, list)->value,
+                        container_of(b, element_t, list)->value) > 0)
+            return false;
+    }
+
+    return true;
+}
+
+/*
+ * Returns a list organized in an intermediate format suited
+ * to chaining of merge() calls: null-terminated, no reserved or
+ * sentinel head node, "prev" links not maintained.
+ */
+
+struct list_head *merge(void *priv,
+                        list_cmp_func_t cmp,
+                        struct list_head *a,
+                        struct list_head *b)
+{
+    struct list_head *head = NULL, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) == true) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+void merge_final(void *priv,
+                 list_cmp_func_t cmp,
+                 struct list_head *head,
+                 struct list_head *a,
+                 struct list_head *b)
+{
+    struct list_head *tail = head;
+    int count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) == true) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        /*
+         * If the merge is highly unbalanced (e.g. the input is
+         * already sorted), this loop may run many iterations.
+         * Continue callbacks to the client even though no
+         * element comparison is needed, so the client's cmp()
+         * routine can invoke cond_resched() periodically.
+         */
+        if (unlikely(!++count))
+            cmp(priv, b, b);
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+void list_sort(void *priv, struct list_head *head, list_cmp_func_t cmp)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    /*
+     * Data structure invariants:
+     * - All lists are singly linked and null-terminated; prev
+     *   pointers are not maintained.
+     * - pending is a prev-linked "list of lists" of sorted
+     *   sublists awaiting further merging.
+     * - Each of the sorted sublists is power-of-two in size.
+     * - Sublists are sorted by size and age, smallest & newest at front.
+     * - There are zero to two sublists of each size.
+     * - A pair of pending sublists are merged as soon as the number
+     *   of following pending elements equals their size (i.e.
+     *   each time count reaches an odd multiple of that size).
+     *   That ensures each later final merge will be at worst 2:1.
+     * - Each round consists of:
+     *   - Merging the two sublists selected by the highest bit
+     *     which flips when count is incremented, and
+     *   - Adding an element from the input as a size-1 sublist.
+     */
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (likely(bits)) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = merge(priv, cmp, b, a);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = merge(priv, cmp, pending, list);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    merge_final(priv, cmp, head, pending, list);
+}
 /* Sort elements of queue in ascending/descending order */
 void q_sort(struct list_head *head, bool descend)
 {
     if (head == NULL || list_empty(head) || list_is_singular(head))
         return;
-    /*
-struct list_head *ni;
-struct list_head *nj;
-struct list_head *nt;
-int size = q_size(head);
-*/
+
     /**
      * list_cut_position() - Move beginning of a list to another list
      * @head_to: pointer to the head of the list which receives nodes
@@ -337,46 +520,16 @@ int size = q_size(head);
      * @head_to is replaced when @head_from is not empty. @node must be a real
      * list node from @head_from or the behavior is undefined.
      */
+    // void *priv = &descend;
+    list_sort(&descend, head, cmp);
 }
-
-
-
-// Use to merge two sorted list
-struct list_head *list_merge(struct list_head *q1,
-                             struct list_head *q2,
-                             bool descend,
-                             int size)
-{
-    /*
-    struct list_head *t1 = q1 ;
-    struct list_head *t2 = q2;
-    struct list_head *n ;
-    if(descend == true){
-        for (t1 = q1->next, t2 =q2->next; t1 != q1 || t2 != q2;) {
-            if (strcmp(container_of(q1, element_t, list)->value,
-            container_of(q2, element_t, list)->value) < 0) {
-
-                list_move(t1,q1);
-                t1 = t1->next;
-
-            }else{
-                list_move(t2, q2);
-                t2 = t2->next;
-            }
-        }
-    }else{
-
-    }*/
-    return q1;
-}
-
-
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
 int q_ascend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
+    // for(struct list_head t = q_ascend ;;;)
 
     return 0;
 }
